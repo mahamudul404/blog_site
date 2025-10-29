a76d172b38b12abc12b3907829244306ba04cd5c
mahamudul404 <mahamudul408432@gmail.com>
2025-10-29 23:57:15 +0600

    route setup

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,16 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [UserController::class, 'home'])->middleware('auth,verified')->name('dashboard');
 
-Route::get('admin/dashboard', function () {
-    return view('admin.dashboard');
-})->middleware(['auth', 'admin'])->name('admin.dashboard');
+Route::get('admin/dashboard' , [UserController::class, 'index'])->middleware('auth,admin')->name('admin.dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
